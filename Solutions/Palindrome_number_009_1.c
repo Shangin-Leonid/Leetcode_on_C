b38369753215e8f8b37cbf18647b0f5c19c5170c
Shangin-Leonid <leonid.shangin@math.msu.ru>
2022-03-11 15:55:13 +0300

    Add Longest_common_prefix_014_1.c , fix code formats in other files

--- Solutions/Palindrome_number_009_1.c
+++ Solutions/Palindrome_number_009_1.c
@@ -1,5 +1,5 @@
 /*
-    Assepted
+    ACCEPTED
     8 ms
     5.8 MB
 */
@@ -9,10 +9,12 @@ bool isPalindrome(int x)
     if(x < 0) return false;
     int arr_of_num[10];
     int order_of_x;
+
     for(order_of_x = 0; x != 0; order_of_x++, x/=10)
         arr_of_num[order_of_x] = x%10;
     for(int i = 0; i < order_of_x/2; i++)
         if(arr_of_num[i] != arr_of_num[order_of_x - 1 - i])
             return false;
+
     return true;
 }
--- Solutions/Two_sum_001_1.c
+++ Solutions/Two_sum_001_1.c
@@ -8,6 +8,7 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize)
 {
     *returnSize = 2;
     int *indices = (int*) malloc(*returnSize * sizeof(int));
+
     for(int i = 0; i < numsSize; i++)
         for(int j = i+1; j < numsSize; j++)
             if(nums[i] + nums[j] == target)
@@ -15,5 +16,6 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize)
                 indices[0] = i;
                 indices[1] = j;
             }
+
     return indices;
 }
